11d5239236b111be7967105cc97d87b82b6c6cae
Strahil Peykov <strahil.peykov@gmail.com>
2025-08-15 13:23:28 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Download, Youtube, AlertCircle, CheckCircle, Loader } from 'lucide-react';
 
 interface VideoInfo {
